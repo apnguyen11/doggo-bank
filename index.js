b12dfb604d565a6f06469dff7e0d13535ff894bd
Andy Nguyen <apnguyen11@yahoo.com>
2019-10-12 13:55:27 -0500

    implemented local login via mongodb and mongoose

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// import { connect } from "tls";
+
 // import { eventNames } from "cluster";
 
 // import { userInfo } from "os";
@@ -6,15 +8,25 @@
 
 // initialize modules
 require('dotenv').config();
-const mustache = require('mustache')
-const fs = require('fs')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const passport = require('passport')
+const mustache = require('mustache');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
+mongoose.connect('mongodb://localhost/DoggoDatabase');
 
 // initialize server
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+    username: String,
+    password: String
+});
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 const port = process.env.PORT || 3000;
@@ -37,9 +49,14 @@ passport.serializeUser(function(user, cb){
     cb(null, user);
 });
 
-passport.deserializeUser(function(Object, cb){
-    cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb){
+//     cb(null, obj);
+// });
+passport.deserializeUser(function(id, cb) {
+    User.findById(id, function(err, user) {
+      cb(err, user);
+    });
+  });
 
 const FacebookStrategy = require('passport-facebook').Strategy;
 
@@ -65,3 +82,31 @@ app.get('/auth/facebook/callback',
     function(req, res){
         res.redirect('/homepage');
     });
+
+// Passport Local Setup
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        UserDetails.findOne({
+          username: username
+        }, function(err, user) {
+          if (err) {
+            return done(err);
+          }
+  
+          if (!user) {
+            return done(null, false);
+          }
+  
+          if (user.password != password) {
+            return done(null, false);
+          }
+          return done(null, user);
+        });
+    }
+  ));
+
+  app.post('/',
+  passport.authenticate('local', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/homepage');
+  });
