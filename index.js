bfacf2d4b2f600f8eb6c023c18e3849ed006f946
Andy Nguyen <apnguyen11@yahoo.com>
2019-10-16 18:09:59 -0500

    incorporated local loginto be functional with users in database

--- index.js
+++ index.js
@@ -15,25 +15,30 @@ const app = express();
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const passport = require('passport');
-const mongoose = require('mongoose');
 const LocalStrategy = require('passport-local').Strategy;
-mongoose.connect('mongodb://localhost/DoggoDatabase');
+
+
+const environment = process.env.NODE_ENV || 'development';
+const dbConfigs = require('./knexfile.js');
+const knex = require('knex')('development');
+const db = require('knex')(dbConfigs.development)
+
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = ('cookie-parser');
+var logger = require('morgan');
 
 // initialize server
-const Schema = mongoose.Schema;
-const UserDetail = new Schema({
-    username: String,
-    password: String
-});
-const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('app listening on port ' + port))
+
 
 // load templates
 const homepageTemplate = fs.readFileSync('./templates/homepage.html', 'utf8')
+const createLogin = fs.readFileSync('./templates/createUser.html', 'utf8')
 
 
 
@@ -41,6 +46,7 @@ const homepageTemplate = fs.readFileSync('./templates/homepage.html', 'utf8')
 app.get('/homepage', (req, res) => {
     res.send(homepageTemplate)
 })
+
 
 // login page
 app.get('/', (req, res) => res.sendFile('auth.html', {root: __dirname}));
@@ -50,6 +56,10 @@ app.get('/error', (req, res) => res.send('error logging in'));
 passport.serializeUser(function(user, cb){
     cb(null, user);
 });
+
+app.get('/createLogin', (req, res) => {
+    res.send(createLogin)
+})
 
 // passport.deserializeUser(function(obj, cb){
 //     cb(null, obj);
@@ -65,51 +75,137 @@ const FacebookStrategy = require('passport-facebook').Strategy;
 const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
 const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
+app.get('/auth/facebook',
+    passport.authenticate('facebook', {scope: 'email'}));
+
+
+
+// function isAuthenticated(rowNum){
+//     if(rowNum == 1){
+//         return true
+//     } else {
+//         return false
+//     }
+// }
 
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: '/auth/facebook/callback'
-},
-function(accessToken, refreshToken, profile, cb){
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'name', 'gender', 'profileUrl', 'emails', 'photos']
+}, function(accessToken, refreshToken, profile, cb){
+    // console.log(profile, '-----------------')
+    findUser('Pete47@gmail.com')
+    .then(function(results){
+        // console.log(results.rows)
+        if(results.rows.length !== 0){
+            // console.log('user exists')
+            throw null
+            return result
+        } else {
+            
+            
+            // app.get('/auth/facebook/callback',
+            // passport.authenticate('facebook', { failureRedirect: '/error'}),
+            // function(req, res){
+            //     res.redirect('/createLogin');
+            // })
+        }
+    })
+
     return cb(null, profile);
-}
+  }
 ));
+
+
+function findUser(email) {
+    // return db.raw('TABLE Users')
+    return db.raw('SELECT * FROM "Users" WHERE email = ?', [email])
+    // .then(function(results){
+    //     console.log(results)
+    //     if(results.length !== 1){
+    //         console.log('user not here')
+    //         throw null
+    //     } else {
+    //         console.log(results)
+    //         return results
+    //     }
+    // })
+};
+
+function createUser(user){
+    return db.raw('INSERT INTO Users (firstName, lastName, address, city, state, zip, email)' [user.firstName, user.lastName, user.address, user.city, user.state, user.zip, user.email])
+  
+}
 
 app.get('/auth/facebook',
     passport.authenticate('facebook'));
 
+
+
 app.get('/auth/facebook/callback',
     passport.authenticate('facebook', { failureRedirect: '/error'}),
-    function(req, res){
-        res.redirect('/homepage');
+    function(req, res, next){
+    //     if(req.isAuthenticated()){
+    //         next()
+    //     } else {
+            res.redirect('/homepage');
+        // }
+        // console.log( req._passport.instance.session)
+        
     });
 
+
+//knex search query
+function findUserByEmail(email){
+    return db.raw('SELECT * FROM "Users" WHERE email = ?', [email])
+}
+
+
+
+
+
 // Passport Local Setup
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        UserDetails.findOne({
-          username: username
-        }, function(err, user) {
-          if (err) {
-            return done(err);
-          }
-  
-          if (!user) {
-            return done(null, false);
-          }
-  
-          if (user.password != password) {
-            return done(null, false);
-          }
+const strategy = new LocalStrategy({
+    username: 'email'
+  },
+  function(email, password, done) {
+    findUserByEmail(email)
+      .then(function(result) {
+          console.log(result.rows, '-------------')
+          var user = result.rows[0];
+          var mappedPassword = result.rows.map(function(rows){
+            return rows.password
+        })
+        console.log(mappedPassword[0], password)
+        if (user && mappedPassword[0] === password) {
           return done(null, user);
-        });
+        } else {
+          return done(null, false);
+        }
+      })
+      .catch(function(err) {
+        console.log('findUserByEmail err:', err);
+        return done(err);
+      });
     }
-  ));
+);
+
+passport.use(strategy);
 
   app.post('/',
   passport.authenticate('local', { failureRedirect: '/error' }),
   function(req, res) {
-    res.redirect('/homepage');
+        res.redirect('/homepage');
   });
 
+
+  app.listen(port, () => {
+    console.log('app listening on port ' + port)
+    // findUser('Pete47@gmail.com')
+    //     .then(function(response) {
+    //         console.log(response);
+    //     }).catch(function (err) {
+    //         console.log(err);
+    //     });
+});
